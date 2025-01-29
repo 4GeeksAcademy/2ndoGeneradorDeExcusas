25922285509b8f98141faac9837d122301840254
JavierML-git <mjavivi673@gmail.com>
2025-01-29 19:11:24 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,34 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let bttn = document.querySelector(".btn");
+  let comando = () => {
+    let randomItem1 = Math.floor(Math.random() * who.length);
+    let randomItem2 = Math.floor(Math.random() * action.length);
+    let randomItem3 = Math.floor(Math.random() * what.length);
+    let randomItem4 = Math.floor(Math.random() * when.length);
+    document.querySelector("#excuse").innerHTML =
+      who[randomItem1] +
+      " " +
+      action[randomItem2] +
+      " " +
+      what[randomItem3] +
+      " " +
+      when[randomItem4];
+  };
+
+  bttn.addEventListener("click", () => {
+    comando();
+  });
 };
